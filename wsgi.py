536aa188c1f3bb10eed4e958ddb04b6767e482f7
Gen_X <104616772+GenX77@users.noreply.github.com>
2025-04-07 10:00:34 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,6 @@
-# wsgi.py placeholder
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decoradores.settings')
+
+application = get_wsgi_application()
